1f0c8519e353c596f7e6c9f948919d6fcc157c63
Joe Wroe <jwroe@thoughtworks.com>
2025-08-12 21:12:38 +0100

    [#4]: Add initial Terraform code
    ∆ Builds a basic hello world Lambda, needs a lot of refactoring

--- app/handlers/statement_ingestor.py
+++ app/handlers/statement_ingestor.py
@@ -1,4 +1,6 @@
-def statement_ingestor(event, context):
+def ingest_statement(event, context):
     # Logic to handle HTTP or S3 trigger
+    print("Hello World")
+
     return {"statusCode": 200, "body": "Success"}
 
---
+++ infra/main.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = "eu-west-2"
+  profile = "banking_apps_dev"
+}
+
+resource "aws_s3_bucket" "statement_csv_bucket" {
+  bucket = "statement-csv-upload-bucket"
+}
+
+resource "aws_iam_role" "statement_ingestor_execution_role" {
+  name = "statement_ingestor_execution_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "statement_ingestor_execution" {
+  role       = aws_iam_role.statement_ingestor_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "statement_ingestor" {
+  function_name = "StatementIngestor"
+  role          = aws_iam_role.statement_ingestor_execution_role.arn
+  handler       = "statement_ingestor.ingest_statement"
+  runtime       = "python3.11"
+  filename      = "${path.module}/lambda/statement_ingestor.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda/statement_ingestor.zip")
+  timeout       = 30
+}
+
+resource "aws_s3_bucket_notification" "statement_bucket_notification" {
+  bucket = aws_s3_bucket.statement_csv_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.statement_ingestor.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = ""
+    filter_suffix       = ".csv"
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
+}
+
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.statement_ingestor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.statement_csv_bucket.arn
+}
